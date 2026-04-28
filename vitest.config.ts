5e28843fd024e6898304a333a58e70437ca2bca5
microshine <microshine@mail.ru>
2026-04-28 20:01:02 +0200

    chore: update coverage configuration

--- vitest.config.ts
+++ vitest.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
       provider: "v8",
       reporter: ["text", "lcov", "text-summary"],
       include: ["src/**/*.ts"],
-      exclude: ["**/*.d.ts"],
+      exclude: ["build/**/*"],
     },
   },
 });
